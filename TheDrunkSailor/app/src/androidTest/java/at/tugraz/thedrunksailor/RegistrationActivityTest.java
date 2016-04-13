9a28a26a88f39a61ac64c5fdd527524cbf1f7fb9
Clemens Stary <c.stary@student.tugraz.at>
2016-04-13 17:02:48 +0200

    added test cases

--- TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/RegistrationActivityTest.java
+++ TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/RegistrationActivityTest.java
@@ -31,7 +31,42 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
 
 
     public void testButtons() {
-        
+        mySolo.clickOnButton("Register");
+
+    }
+
+    public void testTextFields() {
+
+
+        mySolo.clickOnEditText(0);
+
+        String strInput = "Sample code 1";
+
+        mySolo.enterText(0, strInput);
+
+        boolean actual = mySolo.searchEditText(strInput);
+
+        assertEquals("text entered is not matching", true, actual);
+
+
+
+        mySolo.clearEditText(0);
+
+
+        strInput = "Sample code 2";
+        mySolo.typeText(0, strInput);
+        actual = mySolo.searchEditText(strInput);
+
+        assertEquals("text entered is not matching", true, actual);
+
+    }
+
+
+    public void testRadioButtons() {
+
+        mySolo.clickOnRadioButton(1);
+        mySolo.clickOnRadioButton(0);
+
     }
 
 }
