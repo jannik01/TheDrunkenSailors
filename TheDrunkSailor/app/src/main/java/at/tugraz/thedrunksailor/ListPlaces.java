6e981acb985bf8c148d38caaceb50ada756ce37d
David <huemer.d@gmx.at>
2016-04-18 17:41:27 +0200

    [DH, SY] fb_List_Places

--- TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/ListPlaces.java
+++ TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/ListPlaces.java
@@ -1,9 +1,19 @@
 package at.tugraz.thedrunksailor;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,22 +21,38 @@ import java.util.List;
 /**
  * Created by Sami on 18/04/2016.
  */
-public class ListPlaces extends AppCompatActivity{
+public class ListPlaces extends AppCompatActivity {
 
     private List<Place> list_places;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public ListPlaces() {
         setContentView(R.layout.list_places_main);
-        list_places = getPlacesFromDB();
+        Place tmp = new Place("Place 1", 3.4, 7);
+        List<Place> returnPlaces = new ArrayList<Place>();
+        returnPlaces.add(tmp);
+        tmp = new Place("Place 2", 6.5, 2.2);
+        returnPlaces.add(tmp);
+        tmp = new Place("Place 3", 6.1, 2.2);
+        returnPlaces.add(tmp);
+        list_places = returnPlaces;
+
         TableLayout tl = (TableLayout) findViewById(R.id.list_places_table);
-        for(final Place x: list_places) {
-            //ADD ROW CONTENT HERE
+        for (int i = 0; i < list_places.size(); i++) {
+            TableRow row = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            CheckBox checkBox = new CheckBox(this);
+            TextView tv = new TextView(this);
+            TextView qty = new TextView(this);
+            checkBox.setText("hello");
+            qty.setText("10");
+            row.addView(checkBox);
+            row.addView(qty);
+            tl.addView(row, i);
         }
     }
 
-    private List<Place> getPlacesFromDB () {
+    private List<Place> getPlacesFromDB() {
         //TO BE CHANGED ONCE THE DATABASE IS ONLINE
         Place tmp = new Place("Place 1", 3.4, 7);
         List<Place> returnPlaces = new ArrayList<Place>();
@@ -37,5 +63,6 @@ public class ListPlaces extends AppCompatActivity{
         returnPlaces.add(tmp);
         return returnPlaces;
     }
+
 
 }
--- TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/MainActivity.java
+++ TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/MainActivity.java
@@ -3,6 +3,13 @@ package at.tugraz.thedrunksailor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.CheckBox;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Place tmp = new Place("Place 1", 3.4, 7);
+        List<Place> returnPlaces = new ArrayList<Place>();
+        returnPlaces.add(tmp);
+        tmp = new Place("Place 2", 6.5, 2.2);
+        returnPlaces.add(tmp);
+        tmp = new Place("Place 3", 6.1, 2.2);
+        returnPlaces.add(tmp);
+
+        TableLayout tl = (TableLayout) findViewById(R.id.list_places_table);
+        for (int i = 0; i < returnPlaces.size(); i++) {
+            TableRow row = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            for (int j = 0; j < 4; j++) {
+
+                TextView tv = new TextView(this);
+                tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+                //tv.setBackgroundResource(R.drawable.cell_shape);
+                tv.setPadding(5, 5, 5, 5);
+                String tmp_string = new String();
+                switch(j){
+                    case 0: break;
+                    case 1: tmp_string = returnPlaces.get(i).getName(); break;
+                    case 2: tmp_string = Double.toString(returnPlaces.get(i).getCurrent_capacity()); break;
+                    case 3: tmp_string = Double.toString(returnPlaces.get(i).getUser_note()); break;
+                }
+                tv.setText(tmp_string);
+
+                row.addView(tv);
+
+            }
+            tl.addView(row, i);
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
