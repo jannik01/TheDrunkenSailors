6e05b9fd9ae8b8ee3814629a55d6bfea5d878b95
Matthias Moser <m.moser@student.tugraz.at>
2016-04-20 16:54:58 +0200

    Revert "[MM, RH] Error Message Fix"
    This reverts commit 67ad1fabcb6968152c9773dff8e12b76c8182f97.

--- TheDrunkSailor/app/src/androidTest/java/test/SignInLogicTest.java
+++ TheDrunkSailor/app/src/androidTest/java/test/SignInLogicTest.java
@@ -1,46 +1,7 @@
 package test;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.widget.Button;
-
-import com.robotium.solo.Solo;
-import at.tugraz.thedrunksailor.SignInActivity;
-
-//import junit.framework.TestCase;
-
 /**
- * Created by M0S3R on 13.04.2016.
+ * Created by M0S3R on 20.04.2016.
  */
-public class SignInLogicTest extends ActivityInstrumentationTestCase2<SignInActivity>
-{
-
-    private Solo mySolo;
-
-    public SignInLogicTest()
-    {
-        super(SignInActivity.class);
-    }
-
-    public void setUp() throws Exception {
-        super.setUp();
-        mySolo = new Solo(getInstrumentation(), getActivity());
-
-    }
-
-    public void tearDown() throws Exception
-    {
-        super.tearDown();
-    }
-
-    public void testLogic()
-    {
-        Button asdf = mySolo.getButton("Sign in");
-       // asdf.onCli
-
-    }
-
-
-
-
-
+public class SignInLogicTest {
 }
