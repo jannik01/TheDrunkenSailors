8800ad2e4c559c862c8aae27d16f687bb4a0d75c
Clemens Stary <c.stary@student.tugraz.at>
2016-04-20 15:22:07 +0200

    added testcases for registration

--- TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/uitest/RegistrationActivityTest.java
+++ TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/uitest/RegistrationActivityTest.java
@@ -25,6 +25,7 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         super.setUp();
         mySolo = new Solo(getInstrumentation(), getActivity());
 
+
     }
 
     public void tearDown() throws Exception {
@@ -51,7 +52,6 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         assertEquals("text entered is not matching", true, actual);
 
 
-
         mySolo.clearEditText(0);
 
 
@@ -68,6 +68,56 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
 
         mySolo.clickOnRadioButton(1);
         mySolo.clickOnRadioButton(0);
+
+    }
+
+    public void testWrongPassword() throws InterruptedException {
+        mySolo.enterText(0,"username");
+        mySolo.enterText(1,"pwd");
+        mySolo.enterText(2,"pwd1");
+        mySolo.enterText(3,"name");
+
+        mySolo.clickOnButton("Register");
+
+        mySolo.clickOnButton("OK");
+
+    }
+
+
+    public void testMissingField() throws InterruptedException {
+        mySolo.enterText(0,"username");
+        mySolo.enterText(1,"pwd");
+        mySolo.enterText(2,"pwd");
+
+        mySolo.clickOnButton("Register");
+
+        mySolo.clickOnButton("OK");
+
+    }
+
+
+    public void testMissingFieldAndWrongPassword() throws InterruptedException {
+        mySolo.enterText(0,"username");
+        mySolo.enterText(1,"pwd");
+        mySolo.enterText(2,"pwd1");
+
+        mySolo.clickOnButton("Register");
+        mySolo.clickOnButton("OK");
+        mySolo.clickOnButton("OK");
+
+    }
+
+
+    public void testEnterAllFieldsCorrect() {
+        mySolo.enterText(0,"username");
+        mySolo.enterText(1,"pwd");
+        mySolo.enterText(2,"pwd");
+        mySolo.enterText(3,"name");
+        mySolo.clickOnRadioButton(1);
+        mySolo.enterText(4, "34");
+        mySolo.enterText(5, "none");
+
+        mySolo.clickOnButton("Register");
 
     }
 
