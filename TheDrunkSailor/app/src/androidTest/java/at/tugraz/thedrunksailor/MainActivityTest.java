0ff7edf735ddf3e18214f1753ea8a3911d776576
rotho <r.thomann@student.tugraz.at>
2016-04-20 21:36:09 +0200

    [RT,LR] Moved database interface tests to device.

---
+++ TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/MainActivityTest.java
@@ -0,0 +1,24 @@
+package at.tugraz.thedrunksailor;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by robert on 20.04.2016.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2 {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
