dc7e5fde781b63e905b927d78ee73d2386d00602
Clemens Stary <c.stary@student.tugraz.at>
2016-04-20 14:08:30 +0200

    [ES,CS] added restration logic

--- TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/RegistrationActivity.java
+++ TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/RegistrationActivity.java
@@ -1,15 +1,22 @@
 package at.tugraz.thedrunksailor;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     public static String TAG = "REGISTRATION_ACTIVITY";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +26,52 @@ public class RegistrationActivity extends AppCompatActivity {
 
     public void doRegistration(View v) {
 
-        Log.e("REGISTRATION", "btn clicked");
         EditText usernameText = (EditText) findViewById(R.id.txtUsername);
-        EditText passwordText  = (EditText) findViewById(R.id.txtPassword);
-        EditText passwordReText  = (EditText) findViewById(R.id.txtPasswordRe);
-        EditText nameText  = (EditText) findViewById(R.id.txtName);
-        EditText ageText  = (EditText) findViewById(R.id.txtAge);
-        EditText jobText  = (EditText) findViewById(R.id.txtJob);
+        EditText passwordText = (EditText) findViewById(R.id.txtPassword);
+        EditText passwordReText = (EditText) findViewById(R.id.txtPasswordRe);
+        EditText nameText = (EditText) findViewById(R.id.txtName);
+        EditText ageText = (EditText) findViewById(R.id.txtAge);
+        EditText jobText = (EditText) findViewById(R.id.txtJob);
 
         String username = usernameText.getText().toString();
         String password = passwordText.getText().toString();
         String passwordRe = passwordReText.getText().toString();
         String name = nameText.getText().toString();
         String job = jobText.getText().toString();
-        int age = Integer.parseInt(ageText.getText().toString());
+        String gender;
+        int age = 0;
+        RadioButton female = (RadioButton) findViewById(R.id.radioFemale);
+        if (female.isSelected())
+            gender = "f";
+        else
+            gender = "m";
 
-        if(!password.equals(passwordRe)) {
-            Log.d(TAG, "password not the same");
+
+        if (!ageText.getText().toString().equals(""))
+            age = Integer.parseInt(ageText.getText().toString());
+
+        if (!password.equals(passwordRe) || password.equals("") || passwordRe.equals("")) {
+            showAlert("Password Failure", "Password not matching or empty");
         }
 
+        if (username.equals("") || name.equals(""))
+            showAlert("Empty Fields", "Not all required Fields are entered");
 
 
     }
+
+    private void showAlert(String title, String messaage) {
+        AlertDialog alertDialog = new AlertDialog.Builder(RegistrationActivity.this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(messaage);
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+
 }
