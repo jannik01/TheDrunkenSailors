e59a3484f10ba7559400fe578e131eb0cfcfd5b8
Clemens Stary <c.stary@student.tugraz.at>
2016-04-27 16:30:13 +0200

    [DH, CS] navigation and add places form update

---
+++ TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/ActivityAddPlace.java
@@ -0,0 +1,13 @@
+package at.tugraz.thedrunksailor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAddPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_add_place);
+    }
+}
