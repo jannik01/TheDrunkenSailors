a374c1e75ec8ecf8d69e1d00e560b9c4807fcb15
Elisabeth Sprung <elisabeth.sprung@gmail.com>
2016-05-03 13:03:13 +0200

    [ES,MM] adds new view search_place.xml

--- TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/MainActivity.java
+++ TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.search_place);
         DatabaseInterface database_interface_object = new DatabaseInterface();
 
     }
