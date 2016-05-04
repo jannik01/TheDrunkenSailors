5eddcd2dd794a357e5ff6f664854fe24646281c0
Matthias Moser <m.moser@student.tugraz.at>
2016-05-04 14:32:25 +0200

    [MM,ES] Implemented Dropdown

---
+++ TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/SearchPlaceActivityTest.java
@@ -0,0 +1,65 @@
+package at.tugraz.thedrunksailor;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Test;
+import com.robotium.solo.Solo;
+
+/**
+ * Created by robert on 20.04.2016.
+ */
+public class SearchPlaceActivityTest extends ActivityInstrumentationTestCase2<SearchPlaceActivity> {
+
+    public SearchPlaceActivityTest() {
+        super(SearchPlaceActivity.class);
+    }
+    private Solo mySolo;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    @Test
+    public void testButtons() throws InterruptedException {
+
+        mySolo.clickOnButton("Find");
+        mySolo.sleep(5000);
+
+        // TODO testDropdownList - Sector
+
+    }
+
+    public void testTextInput() throws InterruptedException {
+
+        mySolo.enterText(1,"name");
+        mySolo.enterText(2,"8020");
+        mySolo.enterText(3,"place");
+
+        mySolo.clickOnButton("Find");
+        mySolo.sleep(5000);
+
+    }
+
+    public void testRatingBars() throws InterruptedException {
+
+        mySolo.setProgressBar(0, 1);
+        mySolo.sleep(3000);
+        mySolo.setProgressBar(1, 2);
+        mySolo.sleep(3000);
+        mySolo.setProgressBar(2, 5);
+        mySolo.sleep(3000);
+        mySolo.setProgressBar(3, 4);
+        mySolo.sleep(3000);
+
+        mySolo.clickOnButton("Find");
+        mySolo.sleep(5000);
+    }
+
+}
