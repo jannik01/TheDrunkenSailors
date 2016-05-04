ab6c9cfc95b3b3e7bd65cce18adbc4a16fc8aa30
rotho <r.thomann@student.tugraz.at>
2016-05-04 08:46:51 +0200

    [RH,RT] Default database config file.

---
+++ TheDrunkSailorServer/db_config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_USER', "username"); 
+define('DB_PASSWORD', "password"); 
+define('DB_DATABASE', "database name"); 
+define('DB_SERVER', "database_server_url"); 
+define('DB_SERVER_CONNECTION_CHARSET',"utf8mb4_unicode_ci")
+?>
