d99f481f1d3e4134d80f7564f798a8c1c42fc76e
Letscho <leo-reicht@live.com>
2016-05-11 10:06:53 +0200

    [LR,DH] update
    update

--- TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/MainActivity.java
+++ TheDrunkSailor/app/src/main/java/at/tugraz/thedrunksailor/MainActivity.java
@@ -49,12 +49,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-    EditText mPLZ;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_place);
         DatabaseInterface database_interface_object = new DatabaseInterface();
 
 
@@ -77,22 +72,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
 
         }
-    public void searchPlaceLogic(View view)
-    {
-        AlertDialog.Builder alerter = new AlertDialog.Builder(MainActivity.this);
-        mPLZ = (EditText)findViewById(R.id.plz);
-
-        alerter.setIcon(android.R.drawable.ic_dialog_alert);
-
-        String plz = mPLZ.getText().toString();
-
-        if (plz.length() < 3 || plz.length() > 5)
-        {
-            alerter.setMessage("Please put in a zip code between 3 - 5 digits");
-            alerter.setTitle("Fail");
-            alerter.show();
-        }
-
     }
 
 
