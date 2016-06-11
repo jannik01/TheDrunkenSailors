13400edde68bc19868fe873c1496048b28ae621b
Elisabeth Sprung <elisabeth.sprung@gmail.com>
2016-06-11 13:19:18 +0200

    [ES,RT] adds new searchperson fragment test.

---
+++ TheDrunkSailor/app/src/androidTest/java/at/tugraz/thedrunksailor/SearchPerson_FragmentTest.java
@@ -0,0 +1,40 @@
+package at.tugraz.thedrunksailor;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lissy on 11.06.2016.
+ */
+public class SearchPerson_FragmentTest extends ActivityInstrumentationTestCase2<FragmentUtilActivity> {
+
+    private Solo mySolo;
+    private SearchPerson_Fragment searchPerson_Fragment;
+    public SearchPerson_FragmentTest() {
+        super("at.tugraz.thedrunksailor", FragmentUtilActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        searchPerson_Fragment = new SearchPerson_Fragment();
+        getActivity().addFragment(searchPerson_Fragment, SearchPerson_Fragment.class.getSimpleName());
+        getInstrumentation().waitForIdleSync();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testSearchPerson_Fragment(){
+        mySolo.sleep(2000);
+    }
+}
